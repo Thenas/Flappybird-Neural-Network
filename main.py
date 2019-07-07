d3af916a1e68087af377f279c5000d868dadc372
Thenas <adrian_guill@hotmail.com>
2019-07-07 13:37:18 -0600

    reestructura

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import sleep
 import NeuralNet as nNet
 import numpy as np
 
-#Constantes
+#------------------Constantes--------------#
 GRAV = 1
 H = 650
 W = 450
@@ -15,39 +15,36 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 TOTAL = 50
+ARQUI = [2,3,1]
 global GEN;GEN = 0
 
-# Fin Constantes
-
-def nextGen(pajaros, pipes) :
-    global GEN
-    scores = [] #guarda tuplas con el indice del pajaro y su puntuacion 
-    for i in range(len(pajaros)):
-        scores.append((i,pajaros[i].score))
-    scores.sort(reverse = True,key= lambda item:item[1])
-    parents = [pajaros[scores[0][0]],pajaros[scores[1][0]]]
-
-    w1 = np.array(parents[0].nn.weights)
-    w2 = np.array(parents[1].nn.weights)
-
-    wt = (w1+w2)/2
-    pajarosNuevos = []
-
-    for i in range(TOTAL):
-        pajarosNuevos.append(birds(100,rand(25,75)*H/100))
-        pajarosNuevos[i].weights = np.ndarray.tolist(wt * rand(0,100)/100)
-    
-    pipes[0].x = W
-    pipes[1].x = W + 250 
-    pipes[0].y = int(rand(25,75)*H/100)
-    pipes[1].y = int(rand(25,75)*H/100)
-
-    GEN = GEN + 1
-    print("Generacion:",GEN,"          ","PuntacionMax: ", scores[0][1])
-    return pajarosNuevos
-    
+#--------------------------------------------#
 
 
+
+
+#-------------funciones----------------------#
+def crossOver(bird1,bird2):
+    if rand(0,100)>50:
+
+        birdChild = birds(y = bird1.y)
+        birdChild.nn.weights[0] = bird1.nn.weights[0]
+        birdChild.nn.weights[1] = bird2.nn.weights[1]
+
+    else: 
+        birdChild = birds(y = bird2.y)
+        birdChild.nn.weights[0] = bird2.nn.weights[0]
+        birdChild.nn.weights[1] = bird1.nn.weights[1]
+
+
+    return birdChild
+
+
+
+#--------------------------------------------#
+
+
+#---------------Clases-----------------------#
 
 def collition(bird ,pipe):
     Xcollition  = (pipe.x <(bird.x+bird.r)< pipe.x + pipe.ancho) or (pipe.x<(bird.x - bird.r)< pipe.x + pipe.ancho) 
@@ -55,17 +52,14 @@ def collition(bird ,pipe):
     return Xcollition and Ycollition
 
 class birds:
-    def __init__(self, x, y):
-        self.x = x
+    def __init__(self,y):
+        self.x = 100
         self.y = y
-        self.originaly = y
-        self.originalvy = 0.1
         self.r = 20
         self.vy = 0.1
-        self.t = 0
         self.score = 0
         self.fitness = 0
-        self.nn = nNet.NeuralNetwork([6,4,3,2,1],activation="tanh")
+        self.nn = nNet.NeuralNetwork(ARQUI,activation="tanh")
         self.image = pygame.image.load('sprite1.png')
         
     def update(self):
@@ -85,7 +79,7 @@ class birds:
 
 
     def think(self, pipe):
-       X = np.array([self.x,self.y,pipe[0].x,pipe[0].y,pipe[1].x,pipe[1].y])
+       X = np.array([self.y,(pipe.y + (pipe.largo/2))])
        if self.nn.predict(X)>0: self.vy = -10
         
 class pipe:
@@ -107,42 +101,45 @@ class pipe:
         if (self.x+ self.ancho) < 0:
              self.x = W-20
              self.y = int(rand(25,75)*H/100)  
-         
-pajarosMuertos = list() 
-pajaros = list()
-for i in range(TOTAL):pajaros.append(birds(100,rand(25,75)*H/100))
-pipes = [pipe(), pipe(x=W +250)]
 
 
+#--------------------------------------------#   
 
+#--------inicialización de pygame------------#
+ 
 pygame.init()
 win = pygame.display.set_mode((W,H))
 pygame.display.set_caption("fappy bird :V")
+#--------------------------------------------#
 
 
+#----------------setup-----------------------#
+pajaros = []
+pipes = []
+lovePool = []
+
+for i in range(TOTAL):pajaros.append(birds(y=int(rand(10,90)*H/100)))
+for i in range(1,3):pipes.append(pipe(x=W*i))
+
+
+
+#--------------------------------------------#
 while RUN:
     for event in pygame.event.get():
         keyPressed = pygame.key.get_pressed()
         if event.type == pygame.QUIT: RUN = False
-
     win.fill((112, 196, 207))
-    #pygame.time.delay(1)
 
 
-    for Pipe in pipes:
-        for pajaro in pajaros:
-        
-            if collition(pajaro, Pipe): 
-                pajarosMuertos.append(pajaro)
-                pajaros.pop(pajaros.index(pajaro))
-            else: Pipe.color = GREEN
-            pajaro.think(pipes)
-            pajaro.draw(win)
-        Pipe.draw(win) 
-    if len(pajaros) == 0: 
-        pajaros=nextGen(pajarosMuertos,pipes)
-        pajarosMuertos = []
-       
+#-----------------Draw-----------------------#
+    for pajaro in pajaros:
+         pajaro.think(pipes[0])
+         pajaro.draw(win)
+
+#--------------------------------------------#
+
+
+
     pygame.display.update()  
   
 
